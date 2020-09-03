3bcd1ff35a009697acb3ea29483e18b39231418b
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-09-03 13:58:02 -0400

    create CRUD model for api endpoints

--- routers/api/replies.js
+++ routers/api/replies.js
@@ -1,31 +1,94 @@
 /* eslint-disable camelcase */
 const express = require('express');
-const Board = require('../../models/board');
 const Thread = require('../../models/thread');
 const Reply = require('../../models/reply');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/replies/:board').post(async (req, res) => {
-  const { text, delete_password, thread_id } = req.query;
-  const reply = new Reply({ text, delete_password });
+router
+  .route('/replies/:board')
 
-  try {
-    const thread = await Thread.findById(thread_id);
+  // POST a reply to a thread on a specific board
+  .post(async (req, res) => {
+    const { text, delete_password, thread_id } = req.query;
+    const reply = new Reply({ text, delete_password });
 
-    if (!thread) return res.status(404).json({ error: 'thread not found' });
+    try {
+      const thread = await Thread.findById(thread_id);
 
-    await reply.save();
+      if (!thread) return res.status(404).json({ error: 'thread not found' });
 
-    await thread.updateOne(
-      { $push: { replies: reply } },
-      { useFindAndModify: false }
-    );
+      await reply.save();
 
-    return res.status(201).json(reply);
-  } catch (err) {
-    return res.status(400).send(err);
-  }
-});
+      await thread.updateOne(
+        { $push: { replies: reply } },
+        { useFindAndModify: false }
+      );
+
+      return res.status(201).json(reply);
+    } catch (err) {
+      return res.status(400).send(err);
+    }
+  })
+
+  // GET an entire thread with all it's replies
+  .get(async (req, res) => {
+    const { thread_id } = req.query;
+
+    const opts = {
+      path: 'replies',
+      select: '-reported -delete_password -__v',
+    };
+
+    try {
+      const thread = await Thread.findById(thread_id)
+        .select('-reported -delete_password -__v')
+        .populate(opts)
+        .lean();
+
+      if (!thread) return res.status(404).json({ error: 'thread not found' });
+
+      return res.status(200).send(thread);
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  })
+
+  // Report a reply and change it's reported value to true sending a PUT request
+  .put(async (req, res) => {
+    const { reply_id } = req.query;
+
+    try {
+      const reply = await Reply.findByIdAndUpdate(reply_id, {
+        reported: true,
+      });
+
+      if (!reply) return res.status(404).json({ error: 'reply not found' });
+
+      return res.status(200).send('success');
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  })
+
+  // Delete a reply (just changing the text to '[deleted]') sending a DELETE request
+  .delete(async (req, res) => {
+    const { reply_id, delete_password } = req.query;
+
+    try {
+      const reply = await Reply.findById(reply_id);
+
+      if (!reply) return res.status(404).json({ error: 'reply not found' });
+
+      if (reply.delete_password !== delete_password)
+        return res.status(401).send('incorrect password');
+
+      await reply.update({ text: '[deleted]' });
+
+      return res.status(200).send('success');
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  });
 
 module.exports = router;
--- routers/api/threads.js
+++ routers/api/threads.js
@@ -2,28 +2,100 @@
 const express = require('express');
 const Board = require('../../models/board');
 const Thread = require('../../models/thread');
-const Reply = require('../../models/reply');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/threads/:board').post(async (req, res) => {
-  const { board } = req.params;
-  const thread = new Thread(req.query);
+router
+  .route('/threads/:board')
 
-  try {
-    await thread.save();
+  // POST a thread to a specific message board with form data
+  .post(async (req, res) => {
+    const { board } = req.params;
+    const thread = new Thread(req.query);
 
-    // Find board by name and update pushing new issue (If no board is found create one using upsert)
-    await Board.findOneAndUpdate(
-      { name: board },
-      { $push: { threads: thread } },
-      { upsert: true, useFindAndModify: false }
-    );
+    try {
+      await thread.save();
 
-    return res.status(201).json(thread);
-  } catch (err) {
-    return res.status(400).send(err);
-  }
-});
+      // Find board by name and update pushing new issue (If no board is found create one using upsert)
+      await Board.findOneAndUpdate(
+        { name: board },
+        { $push: { threads: thread } },
+        { upsert: true, useFindAndModify: false }
+      );
+
+      return res.status(201).json(thread);
+    } catch (err) {
+      return res.status(400).send(err);
+    }
+  })
+
+  // GET an array of the most recent 10 bumped threads on the board with only the most recent 3 replies
+  .get(async (req, res) => {
+    const { board } = req.params;
+
+    const opts = {
+      path: 'threads',
+      select: '-reported -delete_password -__v',
+      options: {
+        limit: 10,
+        sort: { bumped_on: 'desc' },
+      },
+      populate: {
+        path: 'replies',
+        select: '-reported -delete_password -__v',
+        options: { limit: 3 },
+      },
+    };
+
+    try {
+      const foundBoard = await Board.findOne({ name: board })
+        .populate(opts)
+        .lean();
+
+      if (!foundBoard)
+        return res.status(404).json({ error: 'board not found' });
+
+      return res.status(200).send(foundBoard.threads);
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  })
+
+  // Report a thread and change it's reported value to true sending a PUT request
+  .put(async (req, res) => {
+    const { thread_id } = req.query;
+
+    try {
+      const thread = await Thread.findByIdAndUpdate(thread_id, {
+        reported: true,
+      });
+
+      if (!thread) return res.status(404).json({ error: 'thread not found' });
+
+      return res.status(200).send('success');
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  })
+
+  // Delete a thread completely sending a DELETE request
+  .delete(async (req, res) => {
+    const { thread_id, delete_password } = req.query;
+
+    try {
+      const thread = await Thread.findById(thread_id);
+
+      if (!thread) return res.status(404).json({ error: 'thread not found' });
+
+      if (thread.delete_password !== delete_password)
+        return res.status(401).send('incorrect password');
+
+      await thread.remove();
+
+      return res.status(200).send('success');
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  });
 
 module.exports = router;
