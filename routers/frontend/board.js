883ff8172db5d336ad20fa4c0453914a171b44f9
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-09-04 18:26:29 -0400

    handle post, get, put and delete request

--- routers/frontend/board.js
+++ routers/frontend/board.js
@@ -6,6 +6,20 @@ const router = express.Router({ mergeParams: true });
 router
   .route('/:board')
 
+  .post(async (req, res) => {
+    const { board } = req.params;
+
+    const response = await fetch(`http://localhost:3000/api/threads/${board}`, {
+      method: 'post',
+      body: JSON.stringify(req.body),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    await response.json();
+
+    res.redirect(board);
+  })
+
   .get(async (req, res) => {
     const { board } = req.params;
 
@@ -14,6 +28,34 @@ router
     const threads = await response.json();
 
     res.render('board', { board, threads });
+  })
+
+  .put(async (req, res) => {
+    const { board } = req.params;
+
+    const response = await fetch(`http://localhost:3000/api/threads/${board}`, {
+      method: 'put',
+      body: JSON.stringify(req.body),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    const textResponse = await response.text();
+
+    res.send(textResponse);
+  })
+
+  .delete(async (req, res) => {
+    const { board } = req.params;
+
+    const response = await fetch(`http://localhost:3000/api/threads/${board}`, {
+      method: 'delete',
+      body: JSON.stringify(req.body),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    const textResponse = await response.text();
+
+    res.send(textResponse);
   });
 
 module.exports = router;
